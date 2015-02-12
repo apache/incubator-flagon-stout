5fb4459fe423ce3253031cce954571c8d2648c49
Dan Traviglia <dtraviglia@gmail.com>
2015-02-12 11:10:38 -0500

    updated UX redirection

--- op_tasks/urls.py
+++ op_tasks/urls.py
@@ -6,14 +6,14 @@ from django.contrib.staticfiles import views as vs
 urlpatterns = patterns('',
     # ex: /op_tasks/
     # url(r'^$', index, name='index'),
-    url(r'^task_list/$', views.task_list, name='task_list'),
-    url(r'^product/(?P<seq_pk>[0-9]+)$', views.product, name='product'),
-    url(r'^register/$', views.register),
-    url(r'^login/$', views.login_participant),
-    url(r'^logout/$', views.logout_participant),
-    url(r'^intro/$', views.intro),
-    url(r'^login_intro/$', views.login_intro),
-    url(r'^instruct/$', views.instruct, name='instruct'),
+    url(r'^task_list/$', op_tasks.views.task_list, name='task_list'),
+    url(r'^product/(?P<seq_pk>[0-9]+)$', op_tasks.views.product, name='product'),
+    url(r'^register/$', op_tasks.views.register),
+    url(r'^login/$', op_tasks.views.login_participant),
+    url(r'^logout/$', op_tasks.views.logout_participant),
+    url(r'^intro/$', op_tasks.views.intro),
+    url(r'^login_intro/$', op_tasks.views.login_intro),
+    url(r'^instruct/$', op_tasks.views.instruct, name='instruct'),
 )
 
 print settings.DEBUG, settings.STATIC_ROOT
