25ed51b327ec7c71367d53af5dc03ef4e0a19395
Dan Traviglia <dtraviglia@gmail.com>
2015-02-17 20:59:37 -0500

    first unit test success

--- op_tasks/tests.py
+++ op_tasks/tests.py
@@ -1,10 +1,30 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from op_tasks.views import instruct
+from op_tasks.models import Dataset, Product
 
 # Create your tests here.
 
-class InstructPageTest(TestCase):
-	def test_instruct_url_resolves_to_instruct_page_view(self):
-		found=resolve('op_tasks/instruct/')
-		self.assertEqual(found.func, instruct)
+# class SmokeTest(TestCase):
+
+# 	def test_bad_math(self):
+# 		self.assertEqual(1+1,3)
+
+class ProductModelTest(TestCase):
+
+	def test_saving_and_retrieving_product(self):
+		dataset = Dataset()
+		dataset.version = '1'
+		dataset.name = 'test'
+
+		product = Product()
+		product.dataset = dataset
+		product.url = 'http://espn.go.com'
+		product.team = 'ESPN'
+		product.name = 'ESPNews'
+		product.version = '1'
+		product.instructions = 'http://www.si.com'
+		product.save()
+
+		saved_product = Product.objects.all()
+		self.assertEqual(saved_product.count(), 1)
