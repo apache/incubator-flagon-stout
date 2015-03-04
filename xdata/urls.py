976c6b105bdd23a0d33b44302cb067162f6644c9
Dan Traviglia <dtraviglia@gmail.com>
2015-03-04 11:33:02 -0500

    changed url for experiment dashboard

--- xdata/urls.py
+++ xdata/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	# include urls for op_tasks and administration pages
     url(r'^op_tasks/', include('op_tasks.urls', namespace="op_tasks")),
-    url(r'^exp_portal/', include('exp_portal.urls', namespace="exp_portal")),
+    url(r'^experiment/', include('exp_portal.urls', namespace="exp_portal")),
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^exp_portal/status$', 'exp_portal.views.view_status', name='view_status'),
     (r'^$', index),    
