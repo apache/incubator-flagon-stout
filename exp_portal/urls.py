995f8093dc0dffec8448fc6a10563c21ffe51e0a
Dan Traviglia <dtraviglia@gmail.com>
2015-03-06 16:00:10 -0500

    added submit task to experiment admin

--- exp_portal/urls.py
+++ exp_portal/urls.py
@@ -6,4 +6,7 @@ urlpatterns= patterns('',
 	url(r'^$', 'exp_portal.views.home_page', name='home'),
 	url(r'^status$', 'exp_portal.views.view_status', name='view_status'),
 	url(r'^products$', 'exp_portal.views.view_products', name='view_products'),
+	url(r'^submit$', 'exp_portal.views.submit_task', name='submit_task'),
+	url(r'^newTask$', 'exp_portal.views.new_task', name='new_task'),
+	url(r'^taskAdded$', 'exp_portal.views.task_added', name='task_added'),
 	)
--- exp_portal/views.py
+++ exp_portal/views.py
@@ -15,4 +15,19 @@ def view_status(request):
 
 def view_products(request):
 	products = Product.objects.all()
-	return render(request, 'products.html', {'products': products})	
+	return render(request, 'products.html', {'products': products})
+
+def submit_task(request):
+	return render(request, 'submit_task.html')
+
+def new_task(request):
+	dataset = Dataset.objects.create(name=request.POST['task_dataset'])
+	OpTask.objects.create(dataset=dataset, 
+		survey_url=request.POST['task_url'], 
+		name=request.POST['task_name'], 
+		instructions=request.POST['task_instructions'], 
+		is_active=request.POST['active_check'])
+	return redirect('/experiment/taskAdded/')
+
+def task_added(request):
+	return render(request, 'task_added.html')
