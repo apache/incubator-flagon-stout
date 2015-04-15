603c08e6c6ae6836ced5f061383dd8d82755f799
Dan Traviglia <dtraviglia@gmail.com>
2015-04-15 09:51:53 -0400

    move db and static files outside of main source tree

--- xdata/settings.py
+++ xdata/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 # STATIC_ROOT = "/var/www/html/stout/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 STATIC_URL = '/static/'
 
