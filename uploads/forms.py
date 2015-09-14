93abd3d663f293c237976ec98a24d7484511290f
Kyle Finley <kfinley@draper.com>
2015-09-14 15:18:09 -0400

    Modified uploads files to support loading of a file.

--- uploads/forms.py
+++ uploads/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class DocumentForm(forms.Form):
+	directory = forms.ChoiceField(
+		choices=[('exp1', 'exp1'), ('exp2', 'exp2')]
+	)
 	docfile = forms.FileField(
 		label='Select a file',
 		help_text='max. 42 megabytes'
--- uploads/models.py
+++ uploads/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='data')
+	# DIRECTORY_CHOICES=(
+	# 	('exp1', 'exp1'), 
+	# 	('exp2', 'exp2'),
+	# )
+	# directory = models.CharField(max_length=1, choices=DIRECTORY_CHOICES)
+	docfile = models.FileField(upload_to='data')
