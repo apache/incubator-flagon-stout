52eb35b92efb498f6c56ceb85a13181dedcb29a1
Dan Traviglia <dtraviglia@gmail.com>
2015-09-18 15:50:37 -0400

    upload file to a specific directory built; probably needs improved error checking

--- uploads/views.py
+++ uploads/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from op_tasks.models import Experiment
-from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+import os
 
 from models import Document
 from forms import DocumentForm
@@ -10,22 +11,26 @@ from forms import DocumentForm
 def list(request):
     # Handle file uploads
     if request.method == 'POST':
-        # form = DocumentForm(request.POST, request.FILES)
-        # if form.is_valid():
-        newdoc = Document(docfile = request.FILES['docfile'])
-        newdoc.save()
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES, request.POST.get('dirName', 'error'))
 
         # redirect to the document after POST
         return HttpResponseRedirect(reverse('uploads.views.list'))
 
     else:
         form = DocumentForm() # An empty, unbound form
+        experiments = Experiment.objects.all()
+        # Render list page with the documents and the form
+        return render(request, 'list.html', {'form': form, 'experiments': experiments})
 
-    experiments = Experiment.objects.all()
-
-    # Render list page with the documents and the form
-    return render_to_response(
-        'list.html',
-        {'form': form, 'experiments': experiments},
-        context_instance=RequestContext(request)
-    )
+def handle_uploaded_file(f, dirname):
+    path = os.path.join('../', dirname)
+    try:
+        os.mkdir(path)
+    except:
+        pass
+    file = f['docfile']
+    with open(path  + '/' + file.name, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
